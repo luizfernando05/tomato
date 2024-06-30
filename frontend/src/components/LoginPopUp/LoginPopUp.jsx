395ac3f9c5ddf3d3eef3800b5627961d3b235f2f
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-06-30 14:20:06 -0300

    :sparkles: feat: inserindo funcionalidade de troca de estado no LoginPopUp

--- frontend/src/components/LoginPopUp/LoginPopUp.jsx
+++ frontend/src/components/LoginPopUp/LoginPopUp.jsx
@@ -26,6 +26,21 @@ const LoginPopUp = ({ setShowLogin }) => {
           <input type="password" placeholder="Password" required />
         </div>
         <button>{currState === "Sing Up" ? "Create Account" : "Login"}</button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By continuing, i agree to the terms of use and privacy policy.</p>
+        </div>
+        {currState === "Login" ? (
+          <p>
+            Create a new account?{" "}
+            <span onClick={() => setCurrState("Sing Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <span onClick={() => setCurrState("Login")}>Login here</span>
+          </p>
+        )}
       </form>
     </div>
   );
