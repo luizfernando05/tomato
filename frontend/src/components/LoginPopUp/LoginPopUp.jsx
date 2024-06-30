bfc8d2983dbfe65ba48f18cf99d71eda4dea914c
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-06-30 14:10:34 -0300

    :sparkles: feat: inserindo estrutura inicial para o LoginPopUp

--- frontend/src/components/LoginPopUp/LoginPopUp.jsx
+++ frontend/src/components/LoginPopUp/LoginPopUp.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginPopUp.css";
+import { assets } from "../../assets/frontend_assets/assets";
 
-const LoginPopUp = () => {
-  return <div>LoginPopUp</div>;
+const LoginPopUp = ({ setShowLogin }) => {
+  const [currState, setCurrState] = useState("Sing Up");
+
+  return (
+    <div className="login-pop-up">
+      <form className="login-popup-container">
+        <div className="login-popuup-title">
+          <h2>{currState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt=""
+          />
+        </div>
+        <div className="login-popup-inputs">
+          <input type="text" placeholder="Your name" required />
+          <input type="email" placeholder="Your email" required />
+          <input type="password" placeholder="Password" required />
+        </div>
+        <button>{currState === "Sing Up" ? "Create Account" : "Login"}</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPopUp;
