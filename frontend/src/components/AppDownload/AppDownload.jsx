234bb3502a968a1970cff2e9fcd12ba9590ea2d1
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-06-30 13:14:40 -0300

    :sparkles: feat: inserindo estrutura inicial para o AppDownload

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./AppDownload.css";
+import { assets } from "../../assets/frontend_assets/assets";
 
 const AppDownload = () => {
-  return <div>AppDownload</div>;
+  return (
+    <div className="app-download" id="app-download">
+      <p>
+        For Better Experience Download <br /> Tomato App
+      </p>
+      <div className="app-dowaload-plataforms">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default AppDownload;
