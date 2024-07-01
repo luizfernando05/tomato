c08e769cce4f2eba82970c2031091d0adfaef0da
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-07-01 14:46:15 -0300

    :sparkle: feat: adicionando funcionalidade de neviar a foto para a pasta uploads

--- backend/routes/foodRouter.js
+++ backend/routes/foodRouter.js
@@ -4,6 +4,17 @@ import multer from "multer";
 
 const foodRouter = express.Router();
 
-foodRouter.post("/add", addFood);
+// image store engine
+
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+foodRouter.post("/add", upload.single("image"), addFood);
 
 export default foodRouter;
