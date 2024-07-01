601c7d609b8e1e5dead3dd569fca8cfd65e939fb
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-07-01 13:07:28 -0300

    :sparkles: feat: inserindo redirecionamento do usuário para a página de PlaceOrder

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
     useContext(StoreContext);
+
+  const navigate = useNavigate();
 
   return (
     <div>
@@ -58,7 +61,7 @@ const Cart = () => {
               <b>${getTotalCartAmount() + 2}</b>
             </div>
           </div>
-          <button>PROCED TO CHECKOUT</button>
+          <button onClick={() => navigate("/order")}>PROCED TO CHECKOUT</button>
         </div>
         <div className="cart-promecode">
           <div>
