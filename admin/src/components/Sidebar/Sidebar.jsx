2168520c49e84e3bb8439f9ce4b8c208271b4c23
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-07-02 13:08:58 -0300

    :sparkles: feat: inserindo estrutura inicial do Sidebar

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./Sidebar.css";
+import { assets } from "../../assets/admin_assets/assets";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-options">
+        <div className="sidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Add Itens</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>List Itens</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>Orders</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
