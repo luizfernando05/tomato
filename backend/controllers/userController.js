aa4a4347af3b4316bd82f3fe96ccadd4e45dc8ae
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-07-03 15:45:45 -0300

    :bug: fix: corrigindo erro de digitação no controller do user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,7 +23,7 @@ const loginUser = async (req, res) => {
     }
 
     const token = createToken(user._id);
-    res.json({ sucess: true, token });
+    res.json({ success: true, token });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: "Error" });
