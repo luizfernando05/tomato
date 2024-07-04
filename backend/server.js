1e511f25a55e77ba9fd4238f32dac74bd7649afc
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-07-04 11:19:34 -0300

    :sparkles: feat: inserindo rotas do cart no server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 import foodRouter from "./routes/foodRouter.js";
 import userRouter from "./routes/userRouter.js";
 import "dotenv/config";
+import cartRouter from "./routes/cartRouter.js";
 
 // app config
 const app = express();
@@ -20,6 +21,7 @@ connectDB();
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
