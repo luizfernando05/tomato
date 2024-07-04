f088bfa5c6ac95af090aa7686b1abd2eeb736f98
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-07-04 14:31:43 -0300

    :sparkles: feat: inserindo rota do verify no App.jsx

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIP_SECRET_KEY);
 // placing user order for frontend
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173/";
+  const frontend_url = "http://localhost:5173";
 
   try {
     const newOrder = new orderModel({
