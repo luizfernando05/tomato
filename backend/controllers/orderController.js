b8f785f8d6f3a8826f62ed70fa6d13f424c3f881
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-07-04 13:01:34 -0300

    :sparkles: feat: inserindo controller para o order

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,7 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/UserModel.js";
+import stripe from "stripe";
+
+// placing user order for frontend
+
+const placeOrder = async (req, res) => {};
