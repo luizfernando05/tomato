dd091dce4ee40501871d0285b6940e29befa69bc
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-07-04 14:50:10 -0300

    :sparkles: feat: inserindo estrutura inicial na pagina de Verify

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Verify.css";
+import { useSearchParams } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
 
 const Verify = () => {
-  return <div>Verify</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
+  const { url } = useContext(StoreContext);
+
+  return (
+    <div className="verify">
+      <div className="spinner"></div>
+    </div>
+  );
 };
 
 export default Verify;
