d324f20cae7798e6204f2a38380678a6a9847030
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-07-04 11:15:23 -0300

    :sparkles: feat: inserindo controller do cart

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,15 @@
+import useModel from "../models/UserModel.js";
+
+//  add items to user cart
+
+const addToCart = async (req, res) => {};
+
+//  remove items from user cart
+
+const removeFromCart = async (req, res) => {};
+
+// fetch user cart data
+
+const getCart = async (req, res) => {};
+
+export { addToCart, removeFromCart, getCart };
